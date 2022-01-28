a5ba7df4793e7324577bdfd4e2c885d6cab82742
Mr.Peer <87606844+JunaidPeer@users.noreply.github.com>
2022-01-29 00:04:05 +0530

    0x5005

--- indexScripts.js
+++ indexScripts.js
@@ -1,5 +1,5 @@
 async function addToMetamask() {
 	if (typeof window.ethereum !== "undefined") {
-		await ethereum.request({ method: 'wallet_addEthereumChain', params: [{chainId: '0x-5005',chainName: 'SiriCoin',nativeCurrency:{name: 'SiriCoin',symbol: 'SC',decimals: 18},rpcUrls: ['https://siricoin-node-1.dynamic-dns.net:5005/web3'],blockExplorerUrls :null,}]})
+		await ethereum.request({ method: 'wallet_addEthereumChain', params: [{chainId: '0x5005',chainName: 'SiriCoin',nativeCurrency:{name: 'SiriCoin',symbol: 'SC',decimals: 18},rpcUrls: ['https://siricoin-node-1.dynamic-dns.net:5005/web3'],blockExplorerUrls :null,}]})
 	}
 }
